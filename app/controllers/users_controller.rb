d8af5516f672fd8a532fcfc694acab1479f21475
Rick_Belson <belsonrj@aol.com>
2020-07-06 01:31:23 -0400

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,16 @@
 class UsersController < ApplicationController
+  before_action :require_login
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:user_id])
   end
 
   # GET /users/new
   def new
     @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
   end
 
   # POST /users
@@ -33,35 +25,13 @@ class UsersController < ApplicationController
         #format.html { redirect_to @user, notice: 'User was successfully created.' }
         #format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :new
+        #format.html { render :new }
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     #end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -72,5 +42,9 @@ class UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :password)
+    end
+
+    def require_login
+      return head(:forbidden) unless session.include? :user_id
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :artists
 
   get '/' => 'sessions#welcome'
-  get 'login' => 'sessions#new'
+  #get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'signup' => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
