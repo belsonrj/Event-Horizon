31d013a9eb0e60614cdebf56aa010191838c145d
Rick <belsonrj@aol.com>
2021-03-12 17:19:39 -0500

    deploy1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
+
+    def home
+      render json: {message: "Server is up!"}
+    end
     
     def current_user
        User.find_by(id: session[:user_id])
